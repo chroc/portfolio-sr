530c292a4135bdbaba0e017867e40fd147d8070b
chroc <chroc7@gmail.com>
2022-11-21 19:09:56 +1100

    Social media icons impl

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,6 +5,10 @@ const Footer = () => {
     return (
         <footer>
             <Container>
+                <div className='singleCol footer-icons-box d-flex justify-content-evenly'>
+                    <a href='https://www.linkedin.com/in/sergio-rojas/' target='_blank'><i className="fa-brands fa-linkedin footer-icon"></i></a>
+                    <a href='https://github.com/chroc' target='_blank'><i className="fa-brands fa-github footer-icon"></i></a>
+                </div>
                 <Row>
                     <Col className='text-center py-3'>Copyright &copy; Sergio R.</Col>
                 </Row>
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -14,7 +14,6 @@ const Profile = () => {
                     <h4 className='my-3'>Passionate about coding.</h4>
                     <p className='description'>My sister used to say “Robots are replacing humans, it is going to be hard to find a job in the future.” But I was never worried about it; I would reply, saying “Guess who is going to build those robots then!” I have always loved technology, video games and computers, so the decision about what I wanted to be was always an easy one.</p>
                     <p>Technologies I have been working with recently:</p>
-                    
                         <Row className='my-4 techicons'>
                             <Col className='tech-box' sm={12} md={4} lg={3}>
                                 <i className="fa-brands fa-3x fa-js"></i>
@@ -33,7 +32,6 @@ const Profile = () => {
                                 <h5 className='my-2'>SQL/NoSQL DB</h5>
                             </Col>
                         </Row>
-                    
                 </Col>
             </Row>
         </div>

5ce6fc3834f73c15f43df39b3bb3d69aea862f1f
chroc <chroc7@gmail.com>
2022-11-21 22:54:58 +1100

    Server change path

--- backend/server.js
+++ backend/server.js
@@ -148,11 +148,9 @@ app.post('/api/approve/:email', async (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-path.resolve();
-
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 // Run Server...
